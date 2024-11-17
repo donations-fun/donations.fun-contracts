1f32287d1d135fa45234db712f2dec95bf516c9a
Rares <6453351+raress96@users.noreply.github.com>
2024-11-17 16:28:56 +0200

    Fix analytic tokens and add wrapped avax on fuji.

---
+++ deploy/donate-remove-analytic-tokens.ts
@@ -0,0 +1,37 @@
+import hre from 'hardhat';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { loadConfig, printInfo } from './helpers/basic';
+import { ContractFactory } from 'ethers';
+import { getWallet } from './helpers/utils';
+
+const main = async (hre: HardhatRuntimeEnvironment) => {
+  const Donate = require('#artifacts/contracts/donate/Donate.sol/Donate.json');
+
+  const networkName = hre.network.name;
+
+  const config = loadConfig(networkName);
+
+  const contractConfig = config.contracts?.Donate;
+
+  if (!contractConfig?.address) {
+    throw new Error('Donate contract not deployed yet');
+  }
+
+  const wallet = getWallet(hre);
+
+  let donateFactory = new ContractFactory(Donate.abi, Donate.bytecode, wallet);
+  const contract = donateFactory.attach(contractConfig?.address);
+
+  if (contractConfig?.analyticTokens) {
+    for (const tokenAddress of contractConfig.analyticTokens) {
+      printInfo(`Removing analytic token`, tokenAddress);
+
+      const transaction = await contract.removeAnalyticToken(tokenAddress);
+      await transaction.wait();
+
+      console.log('Sent transaction', transaction.hash);
+    }
+  }
+};
+
+main(hre).catch(console.error);
